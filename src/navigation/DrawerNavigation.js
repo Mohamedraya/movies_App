b3ae504698a13f2aba15e224c5acbccaf2457f54
mohamed Raya <mohamedraya84@gmail.com>
2022-01-13 15:29:41 +0200

    fix issue

--- src/navigation/DrawerNavigation.js
+++ src/navigation/DrawerNavigation.js
@@ -1,27 +1,4 @@
-/*import React from 'react';
-import {createDrawerNavigator} from '@react-navigation/drawer';
-import CustomDrawer from '../components/CustomDrawer';
-import HomeNavigation from "./HomeNavigation";
-import MovieDetailsScreen from '../screens/movieDetails';
-import CastDetailsScreen from '../screens/CastDetails';
-import colors from '../assets/theme/colors';
 
-
-const Drawer = createDrawerNavigator();
-
-function DrawerNavigation  ()  {
-    return (
-        <Drawer.Navigator drawerContent={(props) => {<CustomDrawer {...props}/>}} 
-                          initialRouteName='Home'>
-            <Drawer.Screen name="Home" component={HomeNavigation}/>                
-               
-        </Drawer.Navigator>
-    );
-};
-
-export default DrawerNavigation;*/
-
-///////////////////////
 import React from 'react';
 
 import { createDrawerNavigator } from '@react-navigation/drawer';
